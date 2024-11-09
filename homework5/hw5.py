a520b7336c7ffe7b088f110eaee69f0e8601008a
Alex Besch <148483926+AlexanderBesch@users.noreply.github.com>
2024-11-09 12:38:11 -0600

    created framework for LLMs to debate

--- homework5/hw5.py
+++ homework5/hw5.py
@@ -9,6 +9,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 from transformers import pipeline
 
+import google.generativeai as genai
 
 class LLMs:
     def __init__(self, load_default_api_model=False, load_default_open_source_model=False):
@@ -76,9 +77,99 @@ def test_llms(llms):
     headers = ["#", "Prompt", "Expected Output", "API Model Output", "Open Model Output"]
     print(tabulate.tabulate(table_data, headers=headers, tablefmt="grid"))
 
-if __name__ == "__main__":
-    llms = LLMs(load_default_api_model=True, load_default_open_source_model=True)
 
-    ## Task 1: Test the LLMs
-    test_llms(llms)
+
+
+
+
+class task3E():
+    def __init__(self, api_key_gemini, api_key_openai):
+        # self.api_key = api_key
+        # self.device = device
+        # self.models = {
+        #     "API_model" : None,
+        #     "open_model" : None
+        #     } ## Dictionary to store the models
+        self.gemini_api_key = api_key_gemini
+        self.openai_api_key = api_key_openai
+        self.model_gemini = None
+        self.model_openai = None
+        self.load_gemini_model()
+        self.load_openai_model()
+
+
+    def load_gemini_model(self, model_name='gemini-pro'):
+        genai.configure(api_key=self.gemini_api_key)
+        self.model_gemini = genai.GenerativeModel(model_name)
+    
+    def load_openai_model(self, model_name='gpt-3.5-turbo'):
+        self.model_openai = ChatOpenAI(model=model_name, api_key=self.openai_api_key)
+
+
+    def get_gemini_output(self, system_prompt, other_perspective, initial=False):
+        if initial:
+            response = self.model_gemini.generate_content(system_prompt)
+            return response.text
+        else:
+            # Send a prompt to the model
+            response = self.model_gemini.generate_content(system_prompt + "\n\n" + "Other Perspective: " + other_perspective)
+        # print(response.text)
+        return response.text
+    
+    def get_openai_output(self, system_prompt, other_perspective, initial=False):
+        if initial:
+            response = self.model_openai.invoke(system_prompt)
+            return response.content
+        else:
+            response = self.model_openai.invoke(system_prompt + "\n\n" + "Other Perspective: " + other_perspective)
+        return response.content
+    
+    def have_a_debate(self, system_task, system_prompt_initial_gemini, system_prompt_initial_openai, output_path=None):
+        gemini_output = self.get_gemini_output(system_task + "\n\n" + system_prompt_initial_gemini, "", initial=True)
+        print("Gemini Output: ", gemini_output)
+
+        openai_output = self.get_openai_output(system_task + "\n\n" + system_prompt_initial_openai, "", initial=False)
+        print("OpenAI Output: ", openai_output)
+        if output_path != None:
+            with open(output_path, "w") as f:
+                f.write("Gemini Output: \n" + gemini_output + "\n\n")
+                f.write("Openai Output: \n" + openai_output + "\n\n")
+
+        for i in range(3):
+            gemini_output = self.get_gemini_output(system_task, openai_output)
+            print("OpenAI Output: \n", openai_output)
+            print("\n\n")
+            openai_output = self.get_openai_output(system_task, gemini_output)
+            print("Gemini Output: \n", gemini_output)
+            print("\n\n")
+            if output_path != None:
+                with open(output_path, "a") as f:
+                    f.write("Gemini Output: \n" + gemini_output + "\n\n" + "Openai Output: \n" + openai_output + "\n\n")
+
+if __name__ == "__main__":
+    # llms = LLMs(load_default_api_model=True, load_default_open_source_model=True)
+
+    # ## Task 1: Test the LLMs
+    # test_llms(llms)
+
+
+    # Task 2: Choose the types of prompting we want to use
+    # 1. Meta Prompting
+    # 2. Zero-shot Prompting
+
+
+    # Task 3: Use the Gemini API to generate text
+    AI_debate = task3E(api_key_gemini="See Slack", api_key_openai="See Slack")
+
+    system_task = """You are having a debate with another person that is going to give their perspective on a contriversial topic. 
+                    Please acknowledge the opposing beliefs (if any) and answer their questions. Give a couple reasons why you 
+                    believe what you do, and 1-2 questions to the person you are debating. If you feel that you are being pursuaded 
+                    to the other side, you can say so or begin to shift viewpoints. Keep you answer concise and a single paragraph."""
+    
+    gemini_system_prompt_initial = "You firmly belive pineapple is great on pizza."
+    openai_system_prompt_initial = "You do not belive pineapple belongs on pizza."
+    
+    AI_debate.have_a_debate(system_task, gemini_system_prompt_initial, openai_system_prompt_initial, './homework5/debate.txt')
+    # print(AI_debate.get_gemini_output())
+
 
